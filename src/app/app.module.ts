295ca3896c7ae85198663f5817a2200f607cc214
Valette <floris_valette@hotmail.com>
2019-02-17 22:10:23 +0100

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,12 @@ import { ProductListComponent } from './product-list/product-list.component';
 
 import { RouterModule, Routes, RoutesRecognized } from '@angular/router';
 
-
-const appRoutes: Routes = [
+const appRoutes: Routes=[
   { path: 'products',  component : ProductListComponent },
   { path: 'product/:id',  component : ProductPageComponent },
   { path: '',  redirectTo : 'products', pathMatch: 'full' }
- ] ;
+ ];
  
-
 @NgModule({
   declarations: [
     AppComponent,
